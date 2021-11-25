80712c63747aa9867c0286bab5d26a31c69645e2
Leza Deyzel <lezadeyzel@gmail.com>
2021-11-25 20:17:27 +0200

    Update index.js

--- buildSearchBar/index.js
+++ buildSearchBar/index.js
@@ -9,7 +9,7 @@
 */
 document.getElementById("searchInput").addEventListener("keyup", function(event) {
     let searchQuery = event.target.value.toLowerCase()
-    let allNamesDOMCollection = document.getElementsByClassName('name');
+    let allNamesDOMCollection = document.getElementsByClassName("name");
     
     for (let counter = 0; counter < allNamesDOMCollection.length; counter++) {
         const currentName = allNamesDOMCollection[counter].textContent.toLowerCase()
